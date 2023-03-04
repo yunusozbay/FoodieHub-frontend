c3f95970f24b19e494dac67d9954cd8627543210
zsorosz <oroszi.zsofi@gmail.com>
2023-03-04 20:47:44 +0100

    Add searchbar, show search results

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+
+function SearchBar({ searchTerm, setSearchTerm }) {
+  return (
+    <Form className="d-flex">
+      <Form.Control
+        type="search"
+        placeholder="Find Foodie Friends"
+        className="me-2"
+        aria-label="Search"
+        value={searchTerm}
+        onChange={(event) => setSearchTerm(event.target.value)}
+      />
+      <Button variant="outline-success">Search</Button>
+    </Form>
+  );
+}
+
+export default SearchBar;
